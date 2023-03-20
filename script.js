b9e1876d51617b5e305a182555fb90d918b5586b
Arjesh Khadka <109441944+Arjesh1@users.noreply.github.com>
2023-03-20 21:55:40 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-const billAmt = document.getElementById('bill')
+document.querySelector("#tip-form").addEventListener("click",function(event){
+    event.preventDefault()})
+
+document.querySelector('#tip-form').onchange = function(){
+    let bill = document.getElementById('billAmt').value
+    let tip = document.getElementById('customRange1').value
+    document.getElementById('tipOutput').innerHTML = `${tip}%`;
+
+    let tipAmt = bill * tip/100
+    document.getElementById('tipAmt').innerHTML = tipAmt
+
+    let totalAmt = parseFloat(tipAmt) + parseFloat(bill)
+ 
+    document.getElementById('totalAmt').innerHTML= totalAmt
+
+
+}
